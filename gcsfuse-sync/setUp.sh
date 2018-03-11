e713dd86009ee15d10b3d213ffb70cce7ded4769
Roman Revyakin <revrom+github@gmail.com>
2018-03-11 12:25:19 +1100

    Ability to create subdirectory structure under mount point
    also remove the unused DESTINATION_DIR

--- gcsfuse-sync/setUp.sh
+++ gcsfuse-sync/setUp.sh
@@ -1,8 +1,11 @@
 #!/bin/bash          
 
 mkdir -p $FUSE_MOUNT_DIR
-mkdir -p $DESTINATION_DIR
 gcsfuse -o allow_other $FUSE_BUCKET_NAME $FUSE_MOUNT_DIR
 echo "Mounted succesfully FUSE bucket $FUSE_BUCKET_NAME in $FUSE_MOUNT_DIR"
+for dir in ${SUBDIRECTORY_STRUCTURE}; do
+    echo "Created '$FUSE_MOUNT_DIR/$d' subdirectory"
+    mkdir -p "${FUSE_MOUNT_DIR}/${dir}"
+done
 ls $FUSE_MOUNT_DIR
 tail -f /dev/null
