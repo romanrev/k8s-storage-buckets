9d78f79e0b6246f23cd66589b8f7cf5210f5de56
Roman Revyakin <revrom+github@gmail.com>
2018-03-11 15:45:01 +1100

    Proper name of subdirectory

--- gcsfuse-sync/setUp.sh
+++ gcsfuse-sync/setUp.sh
@@ -4,7 +4,7 @@ mkdir -p $FUSE_MOUNT_DIR
 gcsfuse -o allow_other $FUSE_BUCKET_NAME $FUSE_MOUNT_DIR
 echo "Mounted succesfully FUSE bucket $FUSE_BUCKET_NAME in $FUSE_MOUNT_DIR"
 for dir in ${SUBDIRECTORY_STRUCTURE}; do
-    echo "Created '$FUSE_MOUNT_DIR/$d' subdirectory"
+    echo "Created '$FUSE_MOUNT_DIR/$dir' subdirectory"
     mkdir -p "${FUSE_MOUNT_DIR}/${dir}"
 done
 ls $FUSE_MOUNT_DIR
