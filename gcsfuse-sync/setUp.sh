d4160fd0da29189745a60026b9557c6372a27fed
Roman Revyakin <revrom+github@gmail.com>
2018-03-14 10:32:53 +1100

    Mount individual sub-directories if requested

--- gcsfuse-sync/setUp.sh
+++ gcsfuse-sync/setUp.sh
@@ -1,12 +1,17 @@
 #!/bin/bash          
 
 mkdir -p $FUSE_MOUNT_DIR
-rm -rf "$FUSE_MOUNT_DIR/*"
-gcsfuse -o allow_other,nonempty $FUSE_BUCKET_NAME $FUSE_MOUNT_DIR
-echo "Mounted succesfully FUSE bucket $FUSE_BUCKET_NAME in $FUSE_MOUNT_DIR"
-for dir in ${SUBDIRECTORY_STRUCTURE}; do
-    echo "Created '$FUSE_MOUNT_DIR/$dir' subdirectory"
-    mkdir -p "${FUSE_MOUNT_DIR}/${dir}"
-done
+if [ -n "$SUBDIRECTORY_STRUCTURE" ]; then
+    for dir in ${SUBDIRECTORY_STRUCTURE}; do
+        gs_mount_msg="'gs://$FUSE_BUCKET_NAME/$dir' under '$FUSE_MOUNT_DIR/$dir'"
+        local_mount="${FUSE_MOUNT_DIR}/${dir}"
+        mkdir -p "$local_mount" && echo "Created '$local_mount' subdirectory" || echo "Failed to create '$local_mount'"
+        gcsfuse -o allow_other $FUSE_BUCKET_NAME --only-dir $dir $local_mount && echo "Mounted $gs_mount_msg" || echo "Failed to mount $gs_mount_msg"
+    done
+else
+    local_mount="${FUSE_MOUNT_DIR}"
+    gs_mount_msg="'gs://$FUSE_BUCKET_NAME' under '$FUSE_MOUNT_DIR'"
+    gcsfuse -o allow_other $FUSE_BUCKET_NAME $local_mount && echo "Mounted $gs_mount_msg" || echo "Failed to mount $gs_mount_msg"
+fi
 ls $FUSE_MOUNT_DIR
 tail -f /dev/null
