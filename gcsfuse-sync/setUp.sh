0dc6292f7ae767fe8a0e2ee58863485639122b0c
Roman Revyakin <revrom+github@gmail.com>
2018-03-14 10:10:31 +1100

    Allow nonempty mounts

--- gcsfuse-sync/setUp.sh
+++ gcsfuse-sync/setUp.sh
@@ -2,7 +2,7 @@
 
 mkdir -p $FUSE_MOUNT_DIR
 rm -rf "$FUSE_MOUNT_DIR/*"
-gcsfuse -o allow_other $FUSE_BUCKET_NAME $FUSE_MOUNT_DIR
+gcsfuse -o allow_other,nonempty $FUSE_BUCKET_NAME $FUSE_MOUNT_DIR
 echo "Mounted succesfully FUSE bucket $FUSE_BUCKET_NAME in $FUSE_MOUNT_DIR"
 for dir in ${SUBDIRECTORY_STRUCTURE}; do
     echo "Created '$FUSE_MOUNT_DIR/$dir' subdirectory"
